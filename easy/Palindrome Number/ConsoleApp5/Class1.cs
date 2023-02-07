3703aa6a6b84aadf810e3784debc3a410d13a9ac
lulumayuri <84724511+mayuriswan@users.noreply.github.com>
2023-02-07 23:29:39 +0100

    first commit

---
+++ easy/Palindrome Number/ConsoleApp5/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Class1
+    {
+        
+            public bool IsPalindrome(int x)
+            {
+                int reverse = 0;
+                int l = x;
+                while(l > 0)
+                {
+                
+                int deg = l% 10;
+                l=l/10;
+                reverse *= 10;
+                reverse += deg;
+                Console.WriteLine(reverse + " *********" + deg);
+
+
+                 }
+                 return x == reverse;
+             }
+        
+    }
+}
---
+++ easy/Palindrome Number/ConsoleApp5/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp5;
+
+
+Class1 c = new Class1();
+if (c.IsPalindrome(121))
+{
+    Console.WriteLine("hello");
+}
+else
+{
+    Console.WriteLine("hello Loser");
+}
