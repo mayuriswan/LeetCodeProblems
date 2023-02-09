f749ece05e719be645130b943e98376a57aa3e25
mayuriswan <one12ayoub@gmail.com>
2023-02-09 18:39:01 +0100

    First commit

---
+++ easy/Valid Parentheses/Valid Parentheses/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Valid_Parentheses;
+
+Solution S = new Solution();
+if (S.isValid("([}}])") == true){
+    Console.WriteLine("ga3 streamers zwaml");
+}
+Console.WriteLine("Hello, World!");
---
+++ easy/Valid Parentheses/Valid Parentheses/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Parentheses
+{
+    internal class Solution
+    {
+        public bool isValid(string s)
+        {
+            if (s.Length % 2 != 0 || s[0] == ')' || s[0] == '}' || s[0] == ']')
+            {
+                return false;
+            }
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+                {
+                    stack.Push(s[i]);
+                    Console.WriteLine("push " + s[i]);
+                }
+                else if (stack.Count > 0 && stack.Peek() == '{' && s[i] == '}')
+                {
+                    stack.Pop();
+                    Console.WriteLine("remove " + s[i]);
+                }
+                else if (stack.Count > 0 && stack.Peek() == '[' && s[i] == ']')
+                {
+                    stack.Pop();
+                    Console.WriteLine("remove " + s[i]);
+                }
+                else if (stack.Count > 0 && stack.Peek() == '(' && s[i] == ')')
+                {
+                    stack.Pop();
+                    Console.WriteLine("remove " + s[i] + "3");
+                }
+                else {
+                    stack.Push(s[i]);
+                }
+
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
