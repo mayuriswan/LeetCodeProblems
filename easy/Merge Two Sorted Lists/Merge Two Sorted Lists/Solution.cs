1b4390e50e3add9f15db8321c8e4ad15087f243d
mayuriswan <one12ayoub@gmail.com>
2023-02-10 14:04:26 +0100

    marge two lists

---
+++ easy/Merge Two Sorted Lists/Merge Two Sorted Lists/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_Two_Sorted_Lists
+{
+    internal class Solution
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode result = null;
+            if(list1== null)
+            {
+                return list2;
+            }
+            if(list2== null)
+            {
+                return list1;
+            }
+            if (list1.val >= list2.val)
+            {
+                result.val= list2.val;
+                list1.next= MergeTwoLists(list1.next, list2.next);
+                result.next = list1;
+            }
+            else
+            {
+                result.val = list1.val;
+                list2.next = MergeTwoLists(list1.next, list2.next);
+                result.next = list2;
+            }
+
+
+
+
+            return null;
+        }
+    }
+}
