7efb9d58224704debb27fbf69c382f655fb16a72
mayuriswan <one12ayoub@gmail.com>
2023-02-11 16:33:51 +0100

    ugly number

---
+++ easy/Ugly Number/Ugly Number/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ugly_Number;
+
+Console.WriteLine("Hello, World!");
+Solution s = new Solution();
+if (s.IsUgly(8))
+{
+    Console.WriteLine("right");
+}
+else
+{
+    Console.WriteLine("left");
+}
---
+++ easy/Ugly Number/Ugly Number/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ugly_Number
+{
+    internal class Solution
+    {
+        public bool IsUgly(int n)
+        {
+            if(n<=0) return false;
+            if(n==1) return true;
+            int[] divesers  = new int[] {2,3,5};
+            
+                foreach (int i in divesers)
+                {
+                    while (n % i == 0)
+                    {
+                        n /= i; continue;
+                    }
+                    Console.WriteLine(n);
+                }
+            
+            return n==1;
+        }
+    }
+}
