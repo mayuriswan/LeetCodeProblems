5bf86df9c0bcd96d36900211882056f481a3c2d5
mayuriswan <one12ayoub@gmail.com>
2023-02-11 16:08:32 +0100

    happy number

---
+++ easy/Happy Number/Happy Number/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Happy_Number;
+
+Console.WriteLine("Hello, World!");
+Solution s = new Solution();
+if (s.IsHappy(2))
+{
+    Console.WriteLine("everything working right");
+}
---
+++ easy/Happy Number/Happy Number/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Happy_Number
+{
+    internal class Solution
+    {
+        public int aide(int n)
+        {
+            int resutl   = 0;
+            int d;
+            while (n >0)
+            {
+                d = n % 10;
+                n /=10;
+                resutl += d*d;
+            }
+            Console.WriteLine(resutl);
+            return resutl;
+        }
+        public bool IsHappy(int n)
+        {
+            if(n<=0) return false;
+            HashSet<int> visited = new HashSet<int>();
+            while(n!=1 && !visited.Contains(n))
+            {
+                visited.Add(n);
+                n = aide(n);
+
+            }
+            return n == 1;
+        }
+    }
+}
