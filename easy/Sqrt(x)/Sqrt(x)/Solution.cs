b9b5dbde10fb2e77bd6ab75d708fb800c3e378ae
mayuriswan <one12ayoub@gmail.com>
2023-02-13 20:23:07 +0100

    sqrt(x) and  two Sum

---
+++ easy/Sqrt(x)/Sqrt(x)/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sqrt_x_
+{
+    internal class Solution
+    {
+        public int MySqrt(int x)
+        {
+            if (x == 0) return 0;
+            int low = 1, high = x, ans = 1;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (x / mid == mid) return mid;
+                else if (x / mid > mid)
+                {
+                    low = mid + 1; ans = mid;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ easy/Two Sum/Two Sum/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Sum
+{
+    internal class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> hashmap = new Dictionary<int, int>();
+            int diff = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                diff = target - nums[i];
+                if (hashmap.ContainsKey(diff))
+                {
+                    return new int[] { hashmap[diff], i };
+                }
+                hashmap[nums[i]] = i;
+
+            }
+            return null;
+        }
+    }
+}
